4465ff40cca09e6f062ed230fa236299eaff6b96
pcatalina <pavlovcatalina@gmail.com>
2016-11-30 12:27:12 +0100

    fix arangement of videos

--- replay/views/videos.php
+++ replay/views/videos.php
@@ -4,7 +4,7 @@
     foreach ($v as $video) {
         ?>
         <a href="#">
-        <div class="large-4 columns">
+        <div class="small-12 medium-6 large-4 columns video_image">
         <?php
         echo '<img class="object-fit_none" src="data:image/jpg;base64,' . base64_encode($video->getTexte()) . '"/>';
         ?>
