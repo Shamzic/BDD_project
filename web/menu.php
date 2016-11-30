c4157adf5f5004a17bea347e44956790615477c9
pc-sim\Sim <shamery@free.fr>
2016-11-30 16:20:22 +0200

    delete all unused files

--- web/menu.php
+++
@@ -1,7 +0,0 @@
-<nav>
-	<ul>
-		<li> <a href="index.php">Accueil</a> </li>
-		<li> <a href="connexion.php">Connexion</a> </li>
-		<li> <a href="inscription.php">Inscription</a> </li>
-	</ul>
-</nav>
